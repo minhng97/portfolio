43581cded34ea3045133b59cf73981b609013b7f
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-22 23:05:09 +0700

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import Home from './pages/HomePage/Home'
+import About from './pages/AboutPage/About'
+
 import "./styles.css";
 import 'antd/dist/antd.css';
 
 
-import HomePage from './pages/HomePage/Home'
 
-export default function App() {
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+  
+<>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route component={() => <h1 style={{textAlign: "center", color: "red"}}><Link to="/">404 Not found</Link></h1>} />
+
+          </Switch>
+        </Router>
+</>
+  )}
 }
+
+export default App
