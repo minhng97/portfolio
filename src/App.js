af6a992e7ab0831ad9c58bca20f5eb4c1b465056
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-24 22:45:32 +0700

    Move header and footer from Components to App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import Home from './pages/HomePage/Home'
-import About from './pages/AboutPage/About'
+import Home from "./pages/HomePage/Home"
+import About from "./pages/AboutPage/About"
+import MyWorks from "./pages/MyWorksPage/MyWorks";
+import Contact from "./pages/ContactPage/Contact"
 
 import "./styles.css";
 import 'antd/dist/antd.css';
+import HeaderComponent from "./pages/Header";
+import FooterComponent from "./pages/Footer";
 
 
 
 class App extends React.Component {
   render() {
-  return (
-  
-<>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route component={() => <h1 style={{textAlign: "center", color: "red"}}><Link to="/">404 Not found</Link></h1>} />
+    return (
+
+      <>
+        <Router>
+          <HeaderComponent />
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/myworks" exact component={MyWorks} />
+            <Route path="/contact" exact component={Contact} />
+
+            <Route component={() => <p style={{ textAlign: "center", color: "red", fontSize: "2rem" }}><Link to="/">404 Not found</Link></p>} />
 
           </Switch>
+          <FooterComponent />
+
         </Router>
-</>
-  )}
+      </>
+    )
+  }
 }
 
 export default App
