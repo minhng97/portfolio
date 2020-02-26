d7d396489726087c9ea1b3ca290136ba174d1a1d
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-26 10:44:19 +0700

    My portfo

---
+++ build/precache-manifest.e31ba93c06ad190bcdfc9c25225220a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c060f252482a4e5488470916aa3800fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c83d93f1d53253847c6",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "64bdcf3d043da41d47d0",
+    "url": "/static/css/main.5e8d89ae.chunk.css"
+  },
+  {
+    "revision": "9c83d93f1d53253847c6",
+    "url": "/static/js/2.13e63e14.chunk.js"
+  },
+  {
+    "revision": "64bdcf3d043da41d47d0",
+    "url": "/static/js/main.7f77755f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
