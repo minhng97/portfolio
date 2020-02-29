d3050cccb2ada2d1f562921e434272c670eff118
minhng97 <minh.nguyentrong97@gmail.com>
2020-02-29 18:13:15 +0700

    Updates

---
+++ precache-manifest.d137d3d103c78c32be6cc4dfd711e507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c278c250cd3af856ad4de1bdde93ae3b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a30aa71251b96ed6adc",
+    "url": "/portfolio/static/css/2.dce0b32e.chunk.css"
+  },
+  {
+    "revision": "be609c00a4f90e4115ba",
+    "url": "/portfolio/static/css/main.9659935d.chunk.css"
+  },
+  {
+    "revision": "8a30aa71251b96ed6adc",
+    "url": "/portfolio/static/js/2.44a7ac0e.chunk.js"
+  },
+  {
+    "revision": "be609c00a4f90e4115ba",
+    "url": "/portfolio/static/js/main.54684d34.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b7e294223201cbff227afecb2d576e14",
+    "url": "/portfolio/static/media/main.b7e29422.jpg"
+  }
+]);
